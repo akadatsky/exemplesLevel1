0318a3d7dba844474fbc95dd70a87c1376594afe
andrey.kadatsky <andrey.kadatsky@gmail.com>
2015-03-16 16:15:51 +0200

    remove old data

--- src/com/andreykadatsky/level1/ui/StartActivityRequestActivity.java
+++ src/com/andreykadatsky/level1/ui/StartActivityRequestActivity.java
@@ -22,24 +22,4 @@ public class StartActivityRequestActivity extends Activity {
 
 
     }
-
-    public static class Person implements Serializable {
-
-        private String name;
-        private int age;
-
-        public Person(String name, int age) {
-            this.name = name;
-            this.age = age;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public int getAge() {
-            return age;
-        }
-    }
-
 }
